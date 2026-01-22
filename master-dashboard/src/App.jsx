742550107babfdc9cea59795fae044dc877e4278
AsleshaBorkar <asleshaborkar@gmail.com>
2026-01-22 22:35:32 +0530

    added deployed backend endpoint

--- master-dashboard/src/App.jsx
+++ master-dashboard/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://master-dashboard-wzaq.onrender.com");
 
 function App() {
   const [state, setState] = useState(null);
